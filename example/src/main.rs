a0e731642b108583cef180a2f86131d2c6814226
YdrMaster <ydrml@hotmail.com>
2025-05-07 20:56:03 +0800

    fix: 改正 linear.bias 构建
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,115 +1,21 @@
 mod blob;
 mod gguf;
+mod model;
 
-use blob::Blob;
 use gguf::{GGufModel, map_files};
-use ggus::{GGufMetaMapExt, ggml_quants::digit_layout::types};
+use ggus::ggml_quants::digit_layout::types;
 use nn::{Dim, Exec, GraphBuilder, Node, TensorMeta, op};
 use std::time::Instant;
-use tensor::Tensor;
 
 // cargo run --release -- ../TinyStory-5M-v0.0-F32.gguf
 fn main() {
+    let mut timer = TimeCollector::default();
+
     let path = std::env::args_os().nth(1).unwrap();
     let maps = map_files(path);
     let mut gguf = GGufModel::read(maps.iter().map(|x| &**x));
-    insert_sin_cos(&mut gguf);
-
-    let nvoc = meta![gguf => tokenizer_ggml_tokens].len();
-    let nctx = meta![gguf => llm_context_length];
-    let nblk = meta![gguf => llm_block_count];
-    let d = meta![gguf => llm_embedding_length];
-    let nh = meta![gguf => llm_attention_head_count];
-    let nkvh = meta![gguf => llm_attention_head_count_kv; nh];
-    let dh = meta![gguf => llm_rope_dimension_count; d / nh];
-    let di = meta![gguf => llm_feed_forward_length];
-    let epsilon = meta![gguf => llm_attention_layer_norm_rms_epsilon; 1e-5];
-    let dt_embd = gguf.tensors["token_embd.weight"].dt();
-    let dt_norm = gguf.tensors["output_norm.weight"].dt();
-    let dt_linear = gguf.tensors["output.weight"].dt();
-
-    let t0 = Instant::now();
-    let llama = ::nn::LLaMA {
-        embedding: ::nn::Embedding {
-            dt: dt_embd,
-            d: d.into(),
-            wte: ::nn::Table {
-                row: nvoc.into(),
-                weight: "token_embd.weight".to_string(),
-            },
-            wpe: None,
-        },
-        blks: (0..nblk)
-            .map(|iblk| ::nn::TransformerBlk {
-                attn_norm: ::nn::Normalization {
-                    d: d.into(),
-                    epsilon: epsilon as _,
-                    items: ::nn::NormType::RmsNorm {
-                        dt: dt_norm,
-                        scale: format!("blk.{iblk}.attn_norm.weight"),
-                    },
-                },
-                attn: ::nn::Attention {
-                    nh: nh.into(),
-                    nkvh: nkvh.into(),
-                    qkv: ::nn::Linear {
-                        dt: dt_linear,
-                        shape: [((nh + nkvh + nkvh) * dh).into(), d.into()],
-                        weight: format!("blk.{iblk}.attn_qkv.weight"),
-                        bias: None,
-                    },
-                    rope: Some(::nn::RoPE {
-                        nctx: nctx.into(),
-                        sin: "sin_table".into(),
-                        cos: "cos_table".into(),
-                    }),
-                    output: ::nn::Linear {
-                        dt: dt_linear,
-                        shape: [d.into(), (nh * dh).into()],
-                        weight: format!("blk.{iblk}.attn_output.weight"),
-                        bias: None,
-                    },
-                },
-                ffn_norm: ::nn::Normalization {
-                    d: d.into(),
-                    epsilon: epsilon as _,
-                    items: ::nn::NormType::RmsNorm {
-                        dt: dt_norm,
-                        scale: format!("blk.{iblk}.ffn_norm.weight"),
-                    },
-                },
-                ffn: ::nn::Mlp {
-                    up: ::nn::Linear {
-                        dt: dt_linear,
-                        shape: [(di * 2).into(), d.into()],
-                        weight: format!("blk.{iblk}.ffn_gate_up.weight"),
-                        bias: None,
-                    },
-                    act: ::nn::Activation::SwiGLU,
-                    down: ::nn::Linear {
-                        dt: dt_linear,
-                        shape: [d.into(), di.into()],
-                        weight: format!("blk.{iblk}.ffn_down.weight"),
-                        bias: None,
-                    },
-                },
-            })
-            .collect(),
-        out_norm: ::nn::Normalization {
-            d: d.into(),
-            epsilon: epsilon as _,
-            items: ::nn::NormType::RmsNorm {
-                dt: dt_norm,
-                scale: "output_norm.weight".into(),
-            },
-        },
-        lm_head: ::nn::Linear {
-            dt: dt_linear,
-            shape: [nvoc.into(), d.into()],
-            weight: "output.weight".into(),
-            bias: None,
-        },
-    };
+    let model = model::init(&mut gguf);
+    timer.push("init");
 
     let graph = GraphBuilder::default()
         .register_op("embedding", op::embedding::Embedding)
@@ -123,7 +29,7 @@ fn main() {
         .register_op("rope", op::rope::Rope)
         .register_op("concat", op::concat::Concat)
         .build(
-            llama,
+            model,
             [
                 TensorMeta::new(types::U32, [Dim::var("n")]),
                 TensorMeta::new(types::U32, [Dim::var("n")]),
@@ -131,14 +37,14 @@ fn main() {
             ],
         )
         .unwrap();
+    timer.push("build");
 
-    let t1 = Instant::now();
     let graph = graph.lower(&[("n", 5)].into(), |name| gguf.tensors[&*name].as_ref());
+    timer.push("fix shape");
 
-    let t2 = Instant::now();
     let mem_range_map = graph.mem_range_map(20 << 30, 512);
+    timer.push("alloc");
 
-    let t3 = Instant::now();
     let mut _workspace = vec![0u8; mem_range_map.range.len()];
     let exec = graph
         .lower(
@@ -146,78 +52,39 @@ fn main() {
             |data| data.as_ptr(),
         )
         .into_exec();
+    timer.push("into exec");
 
-    let t4 = Instant::now();
-    println!(
-        "\
-build graph: {:?}
-- nn   : {:?}
-- mem  : {:?}
-- alloc: {:?}
-- exec : {:?}",
-        t4 - t0,
-        t1 - t0,
-        t2 - t1,
-        t3 - t2,
-        t4 - t3,
-    );
+    println!("{timer}");
 
-    for Exec {
-        node,
-        inputs,
-        outputs,
-    } in exec
-    {
-        let Node { op, arg, .. } = node;
+    for Exec { node, .. } in exec {
+        let Node { op, .. } = node;
         match op {
             _ => {}
         }
     }
 }
 
-/// 构造 sin cos 表张量，存储到 GGufModel 中
-fn insert_sin_cos(gguf: &mut GGufModel) {
-    let nctx = meta![gguf => llm_context_length];
-    let d = meta![gguf => llm_embedding_length];
-    let nh = meta![gguf => llm_attention_head_count];
-    let dh = meta![gguf => llm_rope_dimension_count; d / nh];
-    let theta = meta![gguf => llm_rope_freq_base; 1e4];
+#[derive(Default)]
+#[repr(transparent)]
+struct TimeCollector(Vec<(String, Instant)>);
 
-    let ty = types::F32;
-    let mut sin = Blob::new(nctx * dh / 2 * ty.nbytes());
-    let mut cos = Blob::new(nctx * dh / 2 * ty.nbytes());
-
-    {
-        let ([], sin, []) = (unsafe { sin.align_to_mut::<f32>() }) else {
-            unreachable!()
-        };
-        let ([], cos, []) = (unsafe { cos.align_to_mut::<f32>() }) else {
-            unreachable!()
-        };
-        for pos in 0..nctx {
-            for i in 0..dh / 2 {
-                let theta = theta.powf(-((2 * i) as f32 / dh as f32));
-                let freq = pos as f32 * theta;
-                let (sin_, cos_) = freq.sin_cos();
-                sin[pos * dh / 2 + i] = sin_;
-                cos[pos * dh / 2 + i] = cos_;
-            }
-        }
+impl TimeCollector {
+    pub fn push(&mut self, name: impl std::fmt::Display) {
+        self.0.push((name.to_string(), Instant::now()))
     }
+}
 
-    let mut insert = |name, data: Blob| {
-        assert!(
-            gguf.tensors
-                .insert(
-                    name,
-                    Tensor::from_dim_slice(ty, [nctx, dh / 2]).map(|len| {
-                        assert_eq!(len, data.len());
-                        data.into()
-                    })
-                )
-                .is_none()
-        )
-    };
-    insert("sin_table", sin);
-    insert("cos_table", cos);
+impl std::fmt::Display for TimeCollector {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name_width = self.0.iter().map(|(name, _)| name.len()).max().unwrap_or(0) + 2;
+        for i in 1..self.0.len() {
+            writeln!(
+                f,
+                "{:·<name_width$}{:?}",
+                self.0[i].0,
+                self.0[i].1 - self.0[i - 1].1
+            )?
+        }
+        Ok(())
+    }
 }
---
+++ example/src/model.rs
@@ -0,0 +1,161 @@
+﻿use crate::{
+    blob::{Blob, Data},
+    gguf::GGufModel,
+    meta,
+};
+use ggus::GGufMetaMapExt;
+use nn::Tensor;
+use tensor::digit_layout::types;
+
+pub fn init(gguf: &mut GGufModel) -> nn::LLaMA<String> {
+    let arch = meta![gguf => general_architecture];
+    let dt_bias = match arch {
+        "llama" => None,
+        "qwen2" => Some(gguf.tensors["blk.0.attn_qkv.bias"].dt()),
+        arch => panic!("unsupported arch {arch}"),
+    };
+
+    let nvoc = meta![gguf => tokenizer_ggml_tokens].len();
+    let nctx = meta![gguf => llm_context_length];
+    let nblk = meta![gguf => llm_block_count];
+    let d = meta![gguf => llm_embedding_length];
+    let nh = meta![gguf => llm_attention_head_count];
+    let nkvh = meta![gguf => llm_attention_head_count_kv; nh];
+    let dh = meta![gguf => llm_rope_dimension_count; d / nh];
+    let di = meta![gguf => llm_feed_forward_length];
+    let epsilon = meta![gguf => llm_attention_layer_norm_rms_epsilon; 1e-5];
+    let dt_embd = gguf.tensors["token_embd.weight"].dt();
+    let dt_norm = gguf.tensors["output_norm.weight"].dt();
+    let dt_linear = gguf.tensors["blk.0.attn_qkv.weight"].dt();
+    let theta = meta![gguf => llm_rope_freq_base; 1e4];
+
+    let [sin, cos] = build_sin_cos(nctx, dh, theta);
+    gguf.tensors.insert("sin_table", sin);
+    gguf.tensors.insert("cos_table", cos);
+
+    ::nn::LLaMA {
+        embedding: ::nn::Embedding {
+            dt: dt_embd,
+            d: d.into(),
+            wte: ::nn::Table {
+                row: nvoc.into(),
+                weight: "token_embd.weight".to_string(),
+            },
+            wpe: None,
+        },
+        blks: (0..nblk)
+            .map(|iblk| ::nn::TransformerBlk {
+                attn_norm: ::nn::Normalization {
+                    d: d.into(),
+                    epsilon: epsilon as _,
+                    items: ::nn::NormType::RmsNorm {
+                        dt: dt_norm,
+                        scale: format!("blk.{iblk}.attn_norm.weight"),
+                    },
+                },
+                attn: ::nn::Attention {
+                    nh: nh.into(),
+                    nkvh: nkvh.into(),
+                    qkv: ::nn::Linear {
+                        dt: dt_linear,
+                        shape: [((nh + nkvh + nkvh) * dh).into(), d.into()],
+                        weight: format!("blk.{iblk}.attn_qkv.weight"),
+                        bias: dt_bias
+                            .clone()
+                            .map(|dt| (dt, format!("blk.{iblk}.attn_qkv.bias"))),
+                    },
+                    rope: Some(::nn::RoPE {
+                        nctx: nctx.into(),
+                        sin: "sin_table".into(),
+                        cos: "cos_table".into(),
+                    }),
+                    output: ::nn::Linear {
+                        dt: dt_linear,
+                        shape: [d.into(), (nh * dh).into()],
+                        weight: format!("blk.{iblk}.attn_output.weight"),
+                        bias: None,
+                    },
+                },
+                ffn_norm: ::nn::Normalization {
+                    d: d.into(),
+                    epsilon: epsilon as _,
+                    items: ::nn::NormType::RmsNorm {
+                        dt: dt_norm,
+                        scale: format!("blk.{iblk}.ffn_norm.weight"),
+                    },
+                },
+                ffn: ::nn::Mlp {
+                    up: ::nn::Linear {
+                        dt: dt_linear,
+                        shape: [(di * 2).into(), d.into()],
+                        weight: format!("blk.{iblk}.ffn_gate_up.weight"),
+                        bias: None,
+                    },
+                    act: ::nn::Activation::SwiGLU,
+                    down: ::nn::Linear {
+                        dt: dt_linear,
+                        shape: [d.into(), di.into()],
+                        weight: format!("blk.{iblk}.ffn_down.weight"),
+                        bias: None,
+                    },
+                },
+            })
+            .collect(),
+        out_norm: ::nn::Normalization {
+            d: d.into(),
+            epsilon: epsilon as _,
+            items: ::nn::NormType::RmsNorm {
+                dt: dt_norm,
+                scale: "output_norm.weight".into(),
+            },
+        },
+        lm_head: ::nn::Linear {
+            dt: dt_linear,
+            shape: [nvoc.into(), d.into()],
+            weight: if gguf.tensors.contains_key("output.weight") {
+                "output.weight"
+            } else {
+                "token_embd.weight"
+            }
+            .into(),
+            bias: None,
+        },
+    }
+}
+
+/// 构造 sin cos 表张量
+fn build_sin_cos<'a, const N: usize>(
+    nctx: usize,
+    dh: usize,
+    theta: f32,
+) -> [Tensor<Data<'a>, N>; 2] {
+    let ty = types::F32;
+    let mut sin = Blob::new(nctx * dh / 2 * ty.nbytes());
+    let mut cos = Blob::new(nctx * dh / 2 * ty.nbytes());
+
+    {
+        let ([], sin, []) = (unsafe { sin.align_to_mut::<f32>() }) else {
+            unreachable!()
+        };
+        let ([], cos, []) = (unsafe { cos.align_to_mut::<f32>() }) else {
+            unreachable!()
+        };
+        for pos in 0..nctx {
+            for i in 0..dh / 2 {
+                let theta = theta.powf(-((2 * i) as f32 / dh as f32));
+                let freq = pos as f32 * theta;
+                let (sin_, cos_) = freq.sin_cos();
+                sin[pos * dh / 2 + i] = sin_;
+                cos[pos * dh / 2 + i] = cos_;
+            }
+        }
+    }
+
+    let tensor = |data: Blob| {
+        Tensor::from_dim_slice(ty, [nctx, dh / 2]).map(|len| {
+            assert_eq!(len, data.len());
+            data.into()
+        })
+    };
+    [tensor(sin), tensor(cos)]
+}
